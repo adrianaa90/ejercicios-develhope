cd479c8cfa5b390abf615a313bb7ba81892a1d12
Adri <148255735+adrianaa90@users.noreply.github.com>
2023-11-28 17:38:31 +0100

    Objects Copying

--- ej javascript adv/exercise/Objects Copying/exercise/exercise.js
+++ ej javascript adv/exercise/Objects Copying/exercise/exercise.js
@@ -13,3 +13,7 @@ person2.firstName = "Simon";
 
 console.log(person1); 
 console.log(person2); 
+
+
+// cuando hay un igual (=) se crea una referencia al mismo onjeto.
+// Las dos variables pers1 y pers2 apuntan al mismo objeto, por lo tanto si cambiamos una cosa de la otra , la otra tambien se va a ver cambiada.
